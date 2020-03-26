2d8df0988a889923069630bf41906a520b9f7c0c
yangyunpeng <z331047418@aliyun.com>
2020-03-26 23:57:11 +0800

    init

---
+++ src/ServiceFactory.cs
@@ -0,0 +1,48 @@
+﻿using EasyOffice.Enums;
+using EasyOffice.Interfaces;
+using EasyOffice.Models.Word;
+using EasyOffice.Providers.NPOI;
+using EasyOffice.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyOffice
+{
+    public class ServiceFactory
+    {
+        public static IWordExportService GetWordExportService(SolutionEnum solutionEnum= SolutionEnum.NPOI)
+        {
+            switch (solutionEnum)
+            {
+                case SolutionEnum.NPOI:
+                    return new WordExportService(new WordExportProvider());
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+        public static IExcelImportService GetExcelImportService(SolutionEnum solutionEnum = SolutionEnum.NPOI)
+        {
+            switch (solutionEnum)
+            {
+                case SolutionEnum.NPOI:
+                    return new ExcelImportService(new ExcelImportProvider());
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+        public static IExcelExportService GetExcelExportService(SolutionEnum solutionEnum = SolutionEnum.NPOI)
+        {
+            switch (solutionEnum)
+            {
+                case SolutionEnum.NPOI:
+                    return new ExcelExportService(new ExcelExportProvider());
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+    }
+   
+}
